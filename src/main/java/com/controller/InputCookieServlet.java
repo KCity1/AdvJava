1d4849fb8f1e240a50bbf97c6707b234612f1ab0
Vidhit <vidhit.agrawal2004@gmail.com>
2024-09-10 18:19:14 +0530

    InputCookieServlet

---
+++ src/main/java/com/controller/InputCookieServlet.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/InputCookieServlet")
+public class InputCookieServlet extends HttpServlet{
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		System.out.println(name);
+	}
+}
